759e6dd8ef8ddbced4945134164dd353c2943f58
joan compu <isaerlmacha@gmail.com>
2024-02-13 18:54:52 -0500

    nuevo modo

--- script.js
+++ script.js
@@ -24,3 +24,51 @@ function redirectToPage() {
     
     window.location.href = 'inicio.html';
 }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const numHearts = 50; // Número de corazones
+    const corazonesContainer = document.getElementById('corazonesContainer');
+
+    // Función para crear y animar los corazones
+    function createFallingHearts() {
+        for (let i = 0; i < numHearts; i++) {
+            const heart = document.createElement('div');
+            heart.classList.add('corazon');
+            setHeartSize(heart);
+            setInitialPosition(heart);
+            setAnimationDuration(heart);
+
+            corazonesContainer.appendChild(heart);
+
+            // Elimina el corazón después de la animación
+            heart.addEventListener('animationiteration', () => {
+                setInitialPosition(heart);
+            });
+        }
+    }
+
+    // Establece el tamaño del corazón
+    function setHeartSize(heart) {
+        const sizes = ['pequeno', 'mediano', 'grande'];
+        const size = sizes[Math.floor(Math.random() * sizes.length)];
+        heart.classList.add(size);
+    }
+
+    // Establece la posición inicial del corazón
+    function setInitialPosition(heart) {
+        heart.style.left = `${Math.random() * window.innerWidth}px`;
+        heart.style.transform = 'translateY(-90vh)';
+    }
+
+    // Establece la duración de la animación del corazón
+    function setAnimationDuration(heart) {
+        const duration = Math.random() * 5 + 1; // Entre 1 y 3 segundos
+        heart.style.animationDuration = `${duration}s`;
+    }
+
+    // Llama a la función para iniciar la animación
+    createFallingHearts();
+  });
+
